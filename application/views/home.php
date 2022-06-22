29eff6f74532b8654183f526ea30c13ae7e76c0f
Adro312 <daniel.rmz.olv@gmail.com>
2022-06-22 01:55:06 -0500

    Se inicio el proyecto y se configuro para iniciar a trabajar

---
+++ application/views/home.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Website Icons -->
+    <link rel="apple-touch-icon" sizes="76x76" href="<?=base_url('assets/img/icons/IconoCuino.png');?>">
+    <link rel="icon" type="image/png" href="<?=base_url('assets/img/icons/IconoCuino.png');?>">
+
+    <!-- Website Name -->
+    <title>Cuino</title>
+
+    <!-- Principal Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Pacifico&display=swap" rel="stylesheet">
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="<?=base_url('assets/css/nucleo-icons.css');?>" rel="stylesheet" />
+    <link href="<?=base_url('assets/css/nucleo-svg.css');?>" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="<?=base_url('assets/css/nucleo-svg.css');?>" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="<?=base_url('assets/css/argon-dashboard.css?v=2.0.3');?>" rel="stylesheet" />
+    <!-- Own CSS fIle  -->
+    <link rel="stylesheet" href="<?=base_url('assets/css/myStyles.css');?>">
+
+</head>
+<body>
+    <div class="text-center mt-4">
+        <h1>Bienvenido al restaurante Cuino!</h1>
+        <img src="<?=base_url('assets/img/logos/LogoLetrasNegras.png');?>" alt="Image placeholder" class="col-md-3 mt-6">
+        <p class="mt-6">Prueba del segunto tipo de letra que se usara en el programa</p>
+    </div>
+
+    <!--   Core JS Files   -->
+    <script src="<?=base_url('assets/js/core/popper.min.js');?>"></script>
+    <script src="<?=base_url('assets/js/core/bootstrap.min.js');?>"></script>
+    <script src="<?=base_url('assets/js/plugins/perfect-scrollbar.min.js');?>"></script>
+    <script src="<?=base_url('assets/js/plugins/smooth-scrollbar.min.js');?>"></script>
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="<?=base_url('assets/js/argon-dashboard.min.js?v=2.0.3');?>"></script>
+</body>
+</html>
